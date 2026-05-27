ef5da829a449e6972fbabee56204c8006aaad283
d.adhikary.pns@gmail.com <d.adhikary.pns@gmail.com>
2026-05-28 02:10:51 +0530

    add job endpoint

---
+++ PostIQ/src/Services/Published/Published.API/Controllers/JobController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PostIQ.Core.Application.Controllers;
+using Published.Application.Commands;
+
+namespace Published.API.Controllers
+{
+    public class JobController : BaseController
+    {
+        [HttpPost]
+        [Route("Add")]
+        public async Task<IActionResult> Index(AddJobCommand command)
+        {
+            var result = Mediator.Send(command);
+            return Ok(result);
+        }
+    }
+}
---
+++ PostIQ/src/Services/Published/Published.Application/Handlers/AddJobHandler.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using MediatR;
+using PostIQ.Core.Database;
+using PostIQ.Core.Response;
+using Published.Application.Commands;
+using Published.Core.Entities;
+using Published.Core.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Published.Application.Handlers
+{
+    public class AddJobHandler : IRequestHandler<AddJobCommand, CreatedResponse<long>>
+    {
+        private readonly IRepositoryAsync<Job> _job;
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork<PublishDbContext> _uow;
+
+        public AddJobHandler(
+            IUnitOfWork<PublishDbContext> uow,
+            IMapper mapper)
+        {
+            _uow = uow;
+            _job = _uow.GetRepositoryAsync<Job>();
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+        public async Task<CreatedResponse<long>> Handle(AddJobCommand request, CancellationToken cancellationToken)
+        {
+            var entity = _mapper.Map<Job>(request);
+            entity.CreatedBy = request.UserId;
+            entity.CreatedOn = DateTime.UtcNow;
+            entity.IsActive = true;
+
+            await _job.InsertAsync(entity);
+            await _uow.CommitAsync();
+
+            return new CreatedResponse<long>(entity.JobId);
+        }
+    }
+}
