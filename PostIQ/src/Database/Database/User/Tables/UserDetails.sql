8e803ad179f4712f46565f8aea8c59a7b11600c9
d.adhikary.pns@gmail.com <d.adhikary.pns@gmail.com>
2026-06-17 02:58:46 +0530

    #

--- PostIQ/src/Database/Database/User/Tables/UserDetails.sql
+++ PostIQ/src/Database/Database/User/Tables/UserDetails.sql
@@ -5,6 +5,7 @@
     [MiddleName]   VARCHAR (50) NULL,
     [LastName]     VARCHAR (50) NOT NULL,
     [Phone]        VARCHAR (20) NULL,
+    [ReferralCode]  VARCHAR (10) NOT NULL,
     [IsActive]     BIT          NOT NULL,
     [CreatedOn]    DATETIME     NOT NULL,
     [CreatedBy]    BIGINT       NOT NULL,
