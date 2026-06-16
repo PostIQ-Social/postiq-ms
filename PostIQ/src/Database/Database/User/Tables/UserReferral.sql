9d93dd6e5f0261d34862d24530bed05b1f845cd2
d.adhikary.pns@gmail.com <d.adhikary.pns@gmail.com>
2026-06-17 02:52:15 +0530

    #

---
+++ PostIQ/src/Database/Database/User/Tables/UserReferral.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [User].[UserReferral] (
+    [ReferralId] BIGINT      IDENTITY (1, 1) NOT NULL,
+    [UserId]       BIGINT       NOT NULL,
+    [UserName]    VARCHAR (100) NOT NULL,
+    [ReferredById]   BIGINT       NOT NULL,
+    [ReferredByName]    VARCHAR (100) NOT NULL,
+    [IsActive]     BIT          NOT NULL,
+    [CreatedOn]    DATETIME     NULL,
+    [CreatedBy]    BIGINT       NULL,
+    CONSTRAINT [PK_UserReferral] PRIMARY KEY CLUSTERED ([ReferralId] ASC)
+);
+
