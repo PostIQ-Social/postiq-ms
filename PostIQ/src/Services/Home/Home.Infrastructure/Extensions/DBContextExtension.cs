c7e372677eee3af08323fad6f36dc5dfe55eeab8
PARAMITA ROY <roy.paromita1992@gmail.com>
2026-07-28 03:00:53 +0530

    #

--- PostIQ/src/Services/Home/Home.Infrastructure/Extensions/DBContextExtension.cs
+++ PostIQ/src/Services/Home/Home.Infrastructure/Extensions/DBContextExtension.cs
@@ -1,14 +1,8 @@
-﻿using Home.Application.Response;
-using Home.Core.Entities;
-using Home.Core.Persistence;
-using Home.Infrastructure.Jobs.PostSyncJob;
+﻿using Home.Core.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using PostIQ.Core.BackgroundProcess;
-using PostIQ.Core.BackgroundProcess.Interfaces;
 using PostIQ.Core.Database.Extension;
-using PostIQ.Core.HttpClientService.Extensions;
 using PostIQ.Core.Shared.Extensions;
 
 namespace Home.Infrastructure.Extensions
@@ -17,13 +11,6 @@ namespace Home.Infrastructure.Extensions
     {
         public static IServiceCollection AddDbContextExtension(this IServiceCollection services, IConfiguration configuration)
         {
-            // Add HttpClient services
-            services.AddHttpClientService(configuration);
-
-            services.AddBackgroundJob(configuration);
-            services.AddSingleton<IJobItemProcessor<LastBatchJobResponse>, PostSyncJobProcessor>();
-            services.AddSingleton<IJobItemsProducer<LastBatchJobResponse>, PostSyncJobProducer>();
-
             services.AddDbContext<HomeDbContext>(options =>
             {
                 options.UseSqlServer(configuration.GetConnectionString("Default"), o =>
